9a6e303414620f75f33954ad433f6b00c70ab51f
Subhayan Bera <38042918+subhayanbera@users.noreply.github.com>
2018-04-04 22:21:20 +0530

    Update DDL_script1.sql

--- library/SQL/DDL_script1.sql
+++ library/SQL/DDL_script1.sql
@@ -1,38 +1,38 @@
-create table Author(
+create table [bookLibrary].[dbo].Author(
 ID int IDENTITY(1,1) PRIMARY KEY,
 Name varchar(50) NOT NULL,
 [Desc] varchar (200)
 )
 
-create table Category(
+create table [bookLibrary].[dbo].Category(
 ID int IDENTITY(1,1) PRIMARY KEY,
 [Type] varchar(30) NOT NULL UNIQUE,
 [Desc] varchar (200)
 )
 
-create table Publication(
+create table [bookLibrary].[dbo].Publication(
 ID int IDENTITY(1,1) PRIMARY KEY,
 Name varchar(30) NOT NULL UNIQUE,
 Details varchar (200)
 )
 
-create table Roles(
+create table [bookLibrary].[dbo].Roles(
 ID int IDENTITY(1,1) PRIMARY KEY,
 [Type] varchar(20) NOT NULL UNIQUE,
 [Desc] varchar (100)
 )
 
-create table Country(
+create table [bookLibrary].[dbo].Country(
 ID int IDENTITY(1,1) PRIMARY KEY,
 Name varchar(50) NOT NULL UNIQUE
 )
-create table Location(
+create table [bookLibrary].[dbo].Location(
 ID int IDENTITY(1,1) PRIMARY KEY,
 CID INT  FOREIGN KEY references Country(ID),
 Name varchar(50) NOT NULL
 )
 
-create table Book(
+create table [bookLibrary].[dbo].Book(
 ISBN BIGint PRIMARY KEY,
 AuthorID INT  FOREIGN KEY references Author(ID),
 CategoryID INT  FOREIGN KEY references Category(ID),
@@ -44,13 +44,13 @@ Price FLOAT NOT NULL
 
 )
 
-create table EBook(
+create table [bookLibrary].[dbo].EBook(
 ISBN bigint FOREIGN KEY references Book(ISBN) PRIMARY KEY,
 [Path] varchar(50) NOT NULL,
 Format varchar(50) NOT NULL
 )
 
-create table Address(
+create table [bookLibrary].[dbo].Address(
 ID int IDENTITY(1,1) PRIMARY KEY,
 CountryID INT  FOREIGN KEY references Country(ID),
 LocationID INT  FOREIGN KEY references Location(ID),
@@ -67,14 +67,14 @@ AltNo BIGINT NOT NULL
 )
 
 
-create table Library(
+create table [bookLibrary].[dbo].Library(
 ID int IDENTITY(1,1) PRIMARY KEY,
 Name varchar(50) NOT NULL,
 AddressID INT  FOREIGN KEY references Address(ID),
 OwnerName varchar(50)NOT NULL
 )
 
-create table LibraryBook(
+create table [bookLibrary].[dbo].LibraryBook(
 ID int IDENTITY(1,1) PRIMARY KEY,
 ISBN bigINT FOREIGN KEY references Book(ISBN),
 LIbraryID INT FOREIGN KEY references Library(ID),
@@ -83,7 +83,7 @@ InStock Bit NOT NULL,
 AvailableFrom Date NOT NULL,
 )
 
-create table Users(
+create table [bookLibrary].[dbo].Users(
 ID int IDENTITY(1,1) PRIMARY KEY,
 Fname varchar(20) NOT NULL,
 Mname varchar(15),
@@ -94,19 +94,19 @@ LIbraryID INT FOREIGN KEY references Library(ID),
 
 )
 
-create table Creds(
+create table [bookLibrary].[dbo].Creds(
 ID int FOREIGN KEY references Users(ID) PRIMARY KEY,
 UName varchar(20) NOT NULL,
 [Password] varchar(20) NOT NULL
 )
 
-create table UserRole(
+create table [bookLibrary].[dbo].UserRole(
 ID int FOREIGN KEY references Users(ID) PRIMARY KEY,
 UName varchar(20) NOT NULL,
 RollID INT FOREIGN KEY references Roles(ID)
 )
 
-CREATE TABlE Transactions(
+CREATE TABlE [bookLibrary].[dbo].Transactions(
 ID int identity(1,1) Primary KEY,
 UserID int Foreign KEY References Users(ID),
 LbID int FOREIGN KEY REFERENCES Library(ID),
@@ -115,7 +115,7 @@ TDate DATE NOT NULL
 )
 
 
-CREATE TABLE Dues(
+CREATE TABLE [bookLibrary].[dbo].Dues(
 ID INT IDENTITY(1,1) PRIMARY KEY,
 UserID INT FOREIGN KEY REFERENCES Users(ID),
 ISBN BIGINT FOREIGN KEY REFERENCES Book(ISBN),
@@ -126,7 +126,7 @@ HasReturned Bit NOT NULL,
 Condition Varchar(15)
 )
 
-create table Price(
+create table [bookLibrary].[dbo]Price(
 ID int IDENTITY(1,1) PRIMARY KEY,
 [Amount] Float NOT NULL,
 [Duration] Int NOT NULL,
@@ -135,7 +135,7 @@ ID int IDENTITY(1,1) PRIMARY KEY,
 [CountryId] Int FOREIGN KEY references Country(ID)
 )
 
-create table Offer(
+create table  [bookLibrary].[dbo].Offer(
 ID int IDENTITY(1,1) PRIMARY KEY,
 [MTypeID] Int FOREIGN KEY references MembershipType(ID),
 [PriceID] Int FOREIGN KEY references Price(ID),
@@ -145,7 +145,7 @@ ID int IDENTITY(1,1) PRIMARY KEY,
 )
 
 
-create table Coupon(
+create table  [bookLibrary].[dbo].Coupon(
 ID int IDENTITY(1,1) PRIMARY KEY,
 [Code] VARCHAR(20) NOT NULL UNIQUE,
 [MTypeID] Int FOREIGN KEY references MembershipType(ID),
@@ -154,11 +154,12 @@ ID int IDENTITY(1,1) PRIMARY KEY,
 [StartDate] DateTime NOT NULL,
 [EndTime] DateTime NOT NULL,
 [IsBulk] Bit,
-[IsValid] Bit)
+[IsValid] Bit
+)
 
-create table Membership(
+create table  [bookLibrary].[dbo].Membership(
 ID int IDENTITY(1,1) PRIMARY KEY,
-[UserID] Int FOREIGN KEY references User(ID),
+[UserID] Int FOREIGN KEY references Users(ID),
 [MTypeID] Int FOREIGN KEY references MembershipType(ID),
 [StartDate] DateTime NOT NULL,
 [EndTime] DateTime NOT NULL,
@@ -166,16 +167,16 @@ ID int IDENTITY(1,1) PRIMARY KEY,
 )
 
 
-create table MembershipType(
+create table  [bookLibrary].[dbo].MembershipType(
 ID int IDENTITY(1,1) PRIMARY KEY,
-[Name] NOT NULL UNIQUE,
+[Name] VARCHAR(20) NOT NULL UNIQUE,
 [PriceID] Int FOREIGN KEY references Price(ID),
 [DESC] VARCHAR(100)
 )
 
-create table Fine(
+create table  [bookLibrary].[dbo].Fine(
 ID int IDENTITY(1,1) PRIMARY KEY,
-[UserID] Int FOREIGN KEY references User(ID),
+[UserID] Int FOREIGN KEY references Users(ID),
 [DueID] Int FOREIGN KEY references Dues(ID),
 [Amount] FLOAT NOT NULL,
 [IsPaid] Bit NOT NULL,
@@ -183,9 +184,9 @@ ID int IDENTITY(1,1) PRIMARY KEY,
 [DESC] VARCHAR(150)
 )
 
-create table ServiceRequest(
+create table  [bookLibrary].[dbo].ServiceRequest(
 ID int IDENTITY(1,1) PRIMARY KEY,
-[UserID] Int FOREIGN KEY references User(ID),
+[UserID] Int FOREIGN KEY references Users(ID),
 [LBID] Int FOREIGN KEY references LibraryBook(ID),
 [Status] VARCHAR(25) NOT NULL,
 [Agent] VARCHAR(50) NOT NULL,
@@ -193,7 +194,7 @@ ID int IDENTITY(1,1) PRIMARY KEY,
 )
 
 
-create table Payment(
+create table  [bookLibrary].[dbo].Payment(
 ID int IDENTITY(1,1) PRIMARY KEY,
 [FineID] Int FOREIGN KEY references Fine(ID),
 [PriceID] Int FOREIGN KEY references Price(ID),
